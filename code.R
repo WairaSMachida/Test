e3a32dc5424bacafddf523a194ccce3b7da3e26c
Waira S. Machida <wsmbio@gmail.com>
2018-06-29 00:33:56 -0300

    commit da waira

--- code.R
+++ code.R
@@ -1,6 +1,7 @@
 # Nosso código em R
 # By:
 ## Pedro
+## Waira
 
 var1 <- c(1,2,3)
 var2 <- c(3,4,5)
@@ -8,5 +9,6 @@ var2 <- c(3,4,5)
 matr <- matrix(c(var1,var2), ncol=3)
 matr2 <- rbind(var1,var2)
 
+cat <- "gatinho"
 
 
