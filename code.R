6118d26c8924bce1afc63a5b8750c85cfb1a61ef
andre coutinho <andreganemcoutinho@gmail.com>
2018-06-29 00:46:05 -0300

    adicionei um comentario

--- code.R
+++ code.R
@@ -8,7 +8,7 @@ var1 <- c(1,2,3)
 var2 <- c(3,4,5)
 
 matr <- matrix(c(var1,var2), ncol=3)
-matr2 <- rbind(var1,var2)
+matr2 <- rbind(var1,var2) #essa matriz e igual a primeira
 
 var3 <- var1 + var2 #soma
 cat <- "gatinho"
